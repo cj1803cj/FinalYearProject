1b0ae3d3862ac5d4617ac6b46b1bc190ef685bfd
Christopher Cannings <christopher1803cj@gmail.com>
2020-08-18 13:27:08 +0100

    Added user avatar function

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db, login
+from hashlib import md5
 
 # User class
 class User(UserMixin, db.Model):
@@ -16,6 +17,11 @@ class User(UserMixin, db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    # return user avatar from gravatar service
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=retro&s={}'.format(digest, size)
 
     # use __repr__ method to change formatting of printed objects when debugging
     def __repr__(self):
