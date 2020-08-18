5076cdae6599ce84553fba0a27c1f8f2d4fc8671
Christopher Cannings <christopher1803cj@gmail.com>
2020-08-18 16:28:53 +0100

    Added error logging

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
 from config import Config
 
 app = Flask(__name__)
@@ -16,3 +20,15 @@ login.login_view = 'login'
 # import routes at bottom to avoid circular imports as a result
 # of routes also importing the app module
 from app import routes, models, errors
+
+# maintain log for application in logs directory
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/recommendev.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('RecommenDev startup')
