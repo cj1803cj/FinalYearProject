1bb76ed6151b8c273dcd5bc3c4372bd8919080b4
Christopher Cannings <christopher1803cj@gmail.com>
2020-08-18 15:42:48 +0100

    Added route and view to edit profile page

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 from engine import recommend
 
@@ -18,7 +18,7 @@ df = pd.read_csv('TopStaredRepositories.csv', usecols=[1])
 # load model which has already been saved using pickle library
 tfidf_vectorizer = pickle.load(open('tfidf_vectorizer.pickle', 'rb'))
 
-# 
+# call method before processing any requests
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
@@ -112,6 +112,22 @@ def user(username):
         {'owner': user, 'title': 'Project #2'},
     ]
     return render_template('user.html', user=user, projects=projects)
+
+# edit profile endpoint
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
 
 # api endpoint
 @app.route('/api/', methods =['POST'])
