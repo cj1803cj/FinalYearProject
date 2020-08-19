ac7584562d1c0292ac449354e44524a4b3fc82fa
Christopher Cannings <christopher1803cj@gmail.com>
2020-08-19 17:58:59 +0100

    Added reset password form

--- app/forms.py
+++ app/forms.py
@@ -60,3 +60,8 @@ class ProjectForm(FlaskForm):
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
