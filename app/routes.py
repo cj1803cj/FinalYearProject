fd379fabf4065397b1029156b988f5cfe5b6a6d5
Christopher Cannings <christopher1803cj@gmail.com>
2020-08-19 10:25:35 +0100

    Added pagination to projects on user page

--- app/routes.py
+++ app/routes.py
@@ -139,12 +139,14 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    projects = [
-        {'owner': user, 'title': 'Project #1'},
-        {'owner': user, 'title': 'Project #2'},
-    ]
+    page = request.args.get('page', 1, type=int)
+    projects = user.projects.order_by(Project.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=projects.next_num) \
+        if projects.has_next else None
+    prev_url = url_for('user', username=user.username, page=projects.prev_num) \
+        if projects.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, projects=projects, form=form)
+    return render_template('user.html', user=user, projects=projects.items, next_url=next_url, prev_url=prev_url, form=form)
 # end of user profile endpoint
 
 # edit profile endpoint
